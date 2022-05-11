6313b3b4cecf8a1e4eda625d20293c48d068203e
shonentropy3 <103628912+shonentropy3@users.noreply.github.com>
2022-05-11 11:20:59 +0800

    Update CyperStakingV2++.sol

--- CyperStakingV2++.sol
+++ CyperStakingV2++.sol
@@ -4,11 +4,14 @@ pragma solidity >=0.7.0 <0.9.0;
 
 import "@openzeppelin/contracts/utils/math/Math.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/token/ERC1155/utils/ERC1155Receiver.sol";
+import "@openzeppelin/contracts/token/ERC1155/utils/ERC1155Holder.sol";
+import "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
 import "./RewardsDistributionRecipient.sol";
 import "./IStakingRewards.sol";
-import "./Box.sol";
+import "./CyberArms.sol";
+import "./ERC20.sol";
 
 /**pp
  * @dev Users pledge coins to obtain weapons
@@ -17,14 +20,13 @@ import "./Box.sol";
  *
  * _Available since v3.1._                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                         
  */
-contract CyberStakingRewards is RewardsDistributionRecipient, ReentrancyGuard{
+contract CyberStakingRewards is RewardsDistributionRecipient, ReentrancyGuard, ERC1155Holder{
     using SafeMath for uint256;
     CyberArms public CyberArmsRewards;
-    ERC20 public MiddleGoldCoin;
-    ERC20 public GoldCoin;
+    IERC20 public GoldCoin;
     uint256 public periodFinish = 0;
     uint256 public rewardRate = 0;
-    uint256 public rewardsDuration = 1 days;
+    uint256 public rewardsDuration = 30 days;
     uint256 public lastUpdateTime;
     uint256 public rewardPerTokenStored;
     uint256 private _totalSupply;
@@ -35,7 +37,7 @@ contract CyberStakingRewards is RewardsDistributionRecipient, ReentrancyGuard{
     mapping(address => uint256) public rewards;
     // Mapping from account to account GoldCoin been staked
     mapping(address => uint256) private _balances;
-    mapping(uint256 => uint256) private _getPrice;
+    mapping(uint256 => uint256) public _price;
 
     event RewardAdded (uint256 reward);
     event Staked(address indexed user, uint256 amount);
@@ -50,14 +52,15 @@ contract CyberStakingRewards is RewardsDistributionRecipient, ReentrancyGuard{
      */
     constructor (
         address _rewardsDistribution,
-        address _MiddleGoldCoin,
         address _GoldCoin,
         address _CyberArmsRewards
     ) {
-        MiddleGoldCoin = ERC20(_MiddleGoldCoin);
-        GoldCoin = ERC20(_GoldCoin);
+        GoldCoin = IERC20(_GoldCoin);
         CyberArmsRewards = CyberArms(_CyberArmsRewards);
         rewardsDistribution = _rewardsDistribution;
+        _price[1] = 10000;
+        _price[2] = 30000;
+        _price[3] = 60000;
     }
 
      modifier modifierfun(uint256 tokenId, uint256 n){
@@ -102,7 +105,7 @@ contract CyberStakingRewards is RewardsDistributionRecipient, ReentrancyGuard{
      * @dev See {IStakingRewards-earned}.
      */
     function earned(address account) public view returns (uint256) {
-        uint256 earn = _balances[account].mul(rewardPerToken().sub(userRewardPerTokenPaid[account])).div(1e18).add(rewards[account]);
+        uint256 earn = _balances[account].mul(rewardPerToken().sub(userRewardPerTokenPaid[account])).div(1e18).add(rewards[account]);    
         return earn;
     }
 
@@ -114,44 +117,11 @@ contract CyberStakingRewards is RewardsDistributionRecipient, ReentrancyGuard{
     }
 
     /**
-     * @dev See {IStakingRewards-getPrice}.
-     */
-    function getPrice(uint256 tokenId) 
-        public
-        view
-        returns (uint256)
-    {
-        uint256 reward = rewards[msg.sender];
-        uint256 price;
-        if ( tokenId <= 6 && 4 <= tokenId) {
-            price = tokenId*rewardRate*1000;
-            return price;
-        } else if (tokenId <= 3) {
-            price = tokenId*rewardRate;
-            return price;
-        } else {
-            price = tokenId.mul(1000).mul(rewardRate).add(
-                    reward - tokenId
-                    );
-            return _getPrice[tokenId];
-        }
-    }
-
-    /**
      * @dev See {IStakingRewards-DaysNeededPrediction}.
      */
     function DaysNeededPrediction(uint256 tokenId, address account)public view returns(uint256) {
-        uint256 daysNeededNow = (getPrice(tokenId)-earned(account))/(rewardRate);
+        uint256 daysNeededNow = (_price[tokenId].sub(earned(account))).mul(1e18).div(rewardPerToken().mul(_balances[account]));
         return daysNeededNow;
-    }
-    
-    /**
-     * @dev See {IStakingRewards-choose}.
-     */
-    function choose (uint256 k ,uint256 tokenId) internal returns(uint256){
-        uint256 price = getPrice(tokenId);
-        uint256 cost = price.mul(k);
-        return cost;
     }
 
     /**
@@ -168,10 +138,9 @@ contract CyberStakingRewards is RewardsDistributionRecipient, ReentrancyGuard{
      */
     function getNFT (uint256 k, uint256 tokenId) public nonReentrant updateReward(msg.sender){
         uint256 reward = rewards[msg.sender];
-        uint256 cost = choose(k, tokenId);
+        uint256 cost = k.mul(_price[tokenId]);
         rewards[msg.sender] = rewards[msg.sender] - cost;
-        MiddleGoldCoin.transferFrom(msg.sender, address(this), reward);
-        CyberArmsRewards.mint( msg.sender, tokenId, k);
+        CyberArmsRewards.safeTransferFrom(address(this), msg.sender, tokenId, k, "0x7465737400000000000000000000000000000000000000000000000000000000");
         emit Reward(msg.sender, tokenId, k);
     }
 
@@ -182,32 +151,29 @@ contract CyberStakingRewards is RewardsDistributionRecipient, ReentrancyGuard{
         require(amount > 0, "Cannot stake 0");
         _totalSupply = _totalSupply.add(amount);
         _balances[msg.sender] = _balances[msg.sender].add(amount);
-        GoldCoin.approve(address(this), amount);
-        GoldCoin.transferFrom(msg.sender,address(this),amount);
-        emit Staked(msg.sender, amount);
+        GoldCoin.transferFrom(msg.sender, address(this), amount);
+        emit Staked(address(this), amount);
     }
 
     /**
      * @dev See {IStakingRewards-withdraw}.
      */
-    function withdraw(uint256 amount) public  nonReentrant updateReward(msg.sender) {
+    function withdraw(uint256 amount) public nonReentrant updateReward(msg.sender) {
         require(amount > 0, "Cannot withdraw 0");
         _totalSupply = _totalSupply.sub(amount);
         _balances[msg.sender] = _balances[msg.sender].sub(amount);
-        GoldCoin.approve(address(this), amount);
-        GoldCoin.transferFrom(address(this), msg.sender,amount);
+        //GoldCoin.approve(address(this), amount);
+        //GoldCoin.transferFrom(address(this), msg.sender,amount);
         emit Withdrawn(msg.sender, amount);
     }
 
     /**
      * @dev See {IStakingRewards-getReward}.
      */
-    function getReward() public  nonReentrant updateReward(msg.sender) {
+    function getReward() public nonReentrant updateReward(msg.sender) {
         uint256 reward = rewards[msg.sender];
         if (reward > 0) {
             rewards[msg.sender] = 0;
-            MiddleGoldCoin.approve(address(this), reward);
-            MiddleGoldCoin.transferFrom(address(this), msg.sender,reward);
             emit RewardPaid(msg.sender, reward);
         }
     }
@@ -232,7 +198,7 @@ contract CyberStakingRewards is RewardsDistributionRecipient, ReentrancyGuard{
      * - If `to` refers to a smart contract, it must implement {IERC1155Receiver-onERC1155Received} and return the
      * acceptance magic value.
      */
-    function notifyRewardAmount(uint256 reward) external  onlyRewardsDistribution updateReward(address(0)) override{
+    function notifyRewardAmount(uint256 reward) external onlyRewardsDistribution updateReward(address(0)) override{
         if (block.timestamp >= periodFinish) {
             rewardRate = reward.div(rewardsDuration);
         } else {
@@ -240,8 +206,6 @@ contract CyberStakingRewards is RewardsDistributionRecipient, ReentrancyGuard{
             uint256 leftover = remaining.mul(rewardRate);
             rewardRate = reward.add(leftover).div(rewardsDuration);
         }
-        uint balance = MiddleGoldCoin.balanceOf(address(this));
-        require(rewardRate <= balance.div(rewardsDuration), "Provided reward too high");
 
         lastUpdateTime = block.timestamp;
         periodFinish = block.timestamp.add(rewardsDuration);

8c54d3ef287aaf47aa0c0313d8e1cf0d2cf687e6
shonentropy3 <103628912+shonentropy3@users.noreply.github.com>
2022-07-22 19:31:02 +0800

    Delete 3_CYT_migrations.js

--- 3_CYT_migrations.js
+++
@@ -1,9 +0,0 @@
-const CYTStakingRewards2 = artifacts.require("CYTStakingRewards2");
-
-
-module.exports = async function (deployer) {
-      await  deployer.deploy(CYTStakingRewards2,"0x297A03887539581AC84E49807D028Cbdf4350696","0x297A03887539581AC84E49807D028Cbdf4350696");
-      
-      let StakingPoolCoin = await CYTStakingRewards2.deployed();
-      
-};

db15527bc64485b9da61b1ea8d44407f4ce599ae
hellohuangwei <873992313@qq.com>
2022-11-25 16:36:48 +0800

    staking coin

--- migrations/3_CYT_migrations.js
+++
@@ -1,9 +0,0 @@
-const CoinStakingRewards = artifacts.require("CoinStakingRewards");
-
-
-module.exports = async function (deployer) {
-      await  deployer.deploy(CoinStakingRewards,"0xf7fB89554f842F550499AEf4FDa2d1898039851f", "0x297A03887539581AC84E49807D028Cbdf4350696","0x297A03887539581AC84E49807D028Cbdf4350696");
-      
-      let StakingPoolCoin = await CoinStakingRewards.deployed();
-      
-};
